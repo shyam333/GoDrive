98c3db0786d3dadce00f49f043399d7e07dfe163
shyam ramesh <rlshyammdu@gmail.com>
2018-06-05 18:31:25 +0530

    ninenth commit

---
+++ app/src/main/java/helloworld/demo/com/godrive/Appliedjobs.java
@@ -0,0 +1,129 @@
+package helloworld.demo.com.godrive;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by shyamramesh on 24/05/18.
+ */
+
+public class Appliedjobs extends AppCompatActivity {
+
+    List<ListItem> listItem = new ArrayList<>();
+    RecyclerView.Adapter mAdapter;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recycler_view2);
+
+
+        loadRecyclerViewData();
+
+
+
+
+        recyclerView = (RecyclerView)findViewById(R.id.rc2);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+    }
+
+    private void loadRecyclerViewData() {
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(Appliedjobs.this);
+        final String s = preferences.getString("candidateid","n/a");
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading Data....!");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                Constants.URL_APPLIED_JOBS,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            progressDialog.dismiss();
+                            
+                             JSONObject jsonObject = new JSONObject(response);
+                            JSONArray array = jsonObject.getJSONArray("data");
+                            Log.d("response",response.toString());
+                            //JSONArray array = new JSONArray(s);
+                            for (int i = 0; i < array.length(); i++) {
+                                JSONObject o = array.getJSONObject(i);
+                                ListItem item = new ListItem(
+
+                                        o.getString("job_title"),
+                                        o.getString("category_name"),
+                                        o.getString("experience_from"),
+                                        o.getString("experience_to"),
+                                        o.getString("no_of_position"),
+                                        o.getString("location_name"),
+                                        o.getString("budget_from"),
+                                        o.getString("budget_to"),
+                                        o.getString("job_description"),
+                                        o.getString("keyskills"),
+                                        o.getString("education"),
+                                        o.getString("contact_person"),
+                                        o.getString("email_address"),
+                                        o.getString("id")
+                                );
+
+                                listItem.add(item);
+
+                            }
+                            mAdapter = new MyAdapter1(listItem, getApplicationContext());
+                            recyclerView.setAdapter(mAdapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                })
+        {
+        @Override
+        protected Map<String,String> getParams() throws AuthFailureError {
+            Map<String,String>params = new HashMap<>();
+            params.put("candidateid",s);
+            return params;
+        }
+    };
+
+
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
+
+    }
+}
